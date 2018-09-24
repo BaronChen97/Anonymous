ed1c7dcdad73434b93b19b5b38de66d2e6a68bcf
BaronChen97 <42361498+BaronChen97@users.noreply.github.com>
2018-09-24 23:49:41 +1200

    Backgroud
    which adjusts backgroud of the game

---
+++ BackgroundTest2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class BackgroundTest2
+{
+
+    [UnityTest]
+    public IEnumerator Verify_Background_IsRunableTest()
+    {
+        var Background = new GameObject().AddComponent<Background2>();
+
+        bool isBackgroundRun = true;
+
+        yield return null;
+        Assert.AreEqual(true, Background.verifyBackground(isBackgroundRun));
+    }
+}
