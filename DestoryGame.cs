59c08480b7609d7db2b2de23cae86bd9f71c6733
BaronChen97 <42361498+BaronChen97@users.noreply.github.com>
2018-09-24 19:35:52 +1200

    Destroy game
    A feature that when a rocket hits an obstacle, the game will be killed. Also, testing that whether a game will finish or not when the rocket hits an obstacle.

---
+++ DestoryGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DestoryGame : MonoBehaviour {
+
+    public Text gameOverText;
+    private bool isTheGameOver;
+    public GameObject gameController;
+
+    // Use this for initialization
+    void Start () {
+        isTheGameOver = false;
+       // gameOverText.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    //Use  this for trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        isTheGameOver = true;
+        gameOverText.text = "Game Over!!";
+        Destroy(gameObject);
+        gameController.SetActive(false);
+        
+    }
+
+    public bool VerifyGameOver(bool isTheGameOver)
+    {
+        if (isTheGameOver)
+            return true;
+        else
+            return false;
+    }
+
+    
+}
---
+++ Destory_Game_Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Destory_Game_Test {
+
+    [UnityTest]
+    public IEnumerator Destory_Game_TestWithEnumeratorPasses() {
+        var destroyGame = new GameObject().AddComponent<DestoryGame>();
+        bool isTheGameOver = true;
+        
+        yield return null;
+        Assert.AreEqual(true, destroyGame.VerifyGameOver(isTheGameOver));
+    }
+
+}
